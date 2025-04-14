2214a3a6f82e9806b50b4e7e7cf3666e87137182
Moenes98 <amunes.f98@gmail.com>
2025-04-14 22:37:33 +0200

    Remove commands and enhance UI with new icons
    This commit removes several command add-ins from the `HMA.addin` file, including `AddColumnCommand`, `RemoveColumnsCommand`, `ModifyColumnsCommand`, `AnnotateColumnsCommand`, and `AnalyzeColumnsCommand`, resulting in a significant reduction in functionality related to column management.
    
    The `HMA_Addin.csproj` file has been updated to include new icon files, improving the visual representation of commands in the user interface.
    
    In `RibbonManager.cs`, new using directives have been added, and a `PushImage` method has been introduced for more efficient image loading. The ribbon panel now includes a new tab named "HMA Tools," and buttons have been updated to use the new icon paths, enhancing code maintainability and flexibility.
    
    New PNG files for icons (`add.png`, `analyze.png`, `annotate.png`, `ITI logo.png`, `modify.png`, and `remove.png`) have also been added to improve the user experience with better visuals.

--- HMA_Addin/RibbonManager.cs
+++ HMA_Addin/RibbonManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Media.Imaging;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
@@ -9,86 +11,100 @@ namespace HMA_Addin
     [Transaction(TransactionMode.Manual)]
     public class RibbonManager : IExternalApplication
     {
+        string path = Assembly.GetExecutingAssembly().Location;
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
         }
-
         public Result OnStartup(UIControlledApplication application)
         {
-            
-            // Add a new ribbon panel
-            RibbonPanel ribbonPanel = application.CreateRibbonPanel("StrColumns Manager");
+            application.CreateRibbonTab("HMA Tools");
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("HMA Tools", "StrColumns Manager");
 
-            // Create a push button in the ribbon panel
+            #region Buttons
+
             PushButtonData helloButtonData = new PushButtonData(
-                "About", // Button name
-                "About Us", // Button text
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll", // Path to DLL
-                "HMA_Addin.HelloFromHMA" // Fully qualified class name
+                "About",
+                "About Us",
+                path,
+                "HMA_Addin.HelloFromHMA"
             );
-            // Set the large image shown on the button
-            Uri uriImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\ITI logo.png", UriKind.Absolute);
-            BitmapImage largeImage = new BitmapImage(uriImage);
             PushButton helloButton = ribbonPanel.AddItem(helloButtonData) as PushButton;
-            helloButton.LargeImage = largeImage;
+            helloButton.LargeImage = PushImage("HMA_Addin.Icons.ITI logo.png");
 
             PushButtonData createColumnButtonData = new PushButtonData(
                 "StrColumnsCreate",
                 "Create",
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll",
-                "HMA_Addin.CreateColumnsCommand" 
+                path,
+                "HMA_Addin.CreateColumnsCommand"
             );
-            Uri createImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\add.png", UriKind.Absolute);
-            BitmapImage createLargeImage = new BitmapImage(createImage);
             PushButton createColumnButton = ribbonPanel.AddItem(createColumnButtonData) as PushButton;
-            createColumnButton.LargeImage = createLargeImage;
+            createColumnButton.LargeImage = PushImage("HMA_Addin.Icons.add.png");
 
             PushButtonData removeColumnButtonData = new PushButtonData(
                 "StrColumnsRemove",
                 "Remove",
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll",
+                path,
                 "HMA_Addin.RemoveColumnsCommand"
             );
-            Uri removeImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\remove.png", UriKind.Absolute);
-            BitmapImage removeLargeImage = new BitmapImage(removeImage);
             PushButton removeColumnButton = ribbonPanel.AddItem(removeColumnButtonData) as PushButton;
-            removeColumnButton.LargeImage = removeLargeImage;
+            removeColumnButton.LargeImage = PushImage("HMA_Addin.Icons.remove.png");
 
             PushButtonData modifyColumnButtonData = new PushButtonData(
                 "StrColumnsModify",
                 "Move",
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll",
+                path,
                 "HMA_Addin.ModifyColumnsCommand"
             );
-            Uri modifyImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\modify.png", UriKind.Absolute);
-            BitmapImage modifyLargeImage = new BitmapImage(modifyImage);
             PushButton modifyColumnButton = ribbonPanel.AddItem(modifyColumnButtonData) as PushButton;
-            modifyColumnButton.LargeImage = modifyLargeImage;
+            modifyColumnButton.LargeImage = PushImage("HMA_Addin.Icons.modify.png");
 
+            //"annotate" button
             PushButtonData annotateColumnButtonData = new PushButtonData(
                 "Add Tags",
                 "Annotate",
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll",
+                path,
                 "HMA_Addin.AnnotateColumnsCommand"
             );
-            Uri annotateImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\annotate.png", UriKind.Absolute);
-            BitmapImage annotateLargeImage = new BitmapImage(annotateImage);
             PushButton annotateColumnButton = ribbonPanel.AddItem(annotateColumnButtonData) as PushButton;
-            annotateColumnButton.LargeImage = annotateLargeImage;
+            annotateColumnButton.LargeImage = PushImage("HMA_Addin.Icons.annotate.png");
 
+            //"analyze" button
             PushButtonData analyzeColumnButtonData = new PushButtonData(
                 "Create Sheets",
                 "Analyze",
-                @"C:\Users\Ahmed Moenes\AppData\Roaming\Autodesk\Revit\Addins\2022\HMA_Addin.dll",
+                path,
                 "HMA_Addin.AnalyzeColumnsCommand"
             );
-            Uri analyzeImage = new Uri(@"C:\Users\Ahmed Moenes\Desktop\analyze.png", UriKind.Absolute);
-            BitmapImage analyzeLargeImage = new BitmapImage(analyzeImage);
             PushButton analyzeColumnButton = ribbonPanel.AddItem(analyzeColumnButtonData) as PushButton;
-            analyzeColumnButton.LargeImage = analyzeLargeImage;
+            analyzeColumnButton.LargeImage = PushImage("HMA_Addin.Icons.analyze.png");
+            #endregion
 
             return Result.Succeeded;
+        }
+        private BitmapImage PushImage(string resourcePath)
+        {
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+                {
+                    if (stream == null)
+                        throw new Exception($"Image not found: {resourcePath}");
+
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+                    return image;
+                }
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error", $"Failed to load image: {ex.Message}");
+                return null;
+            }
         }
     }
 
